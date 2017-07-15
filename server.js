61ec6d04dda6c958d7e7e3f24c89d6320825fdff
cecolby11 <cecolby11@users.noreply.github.com>
2017-07-14 22:32:04 -0500

    mongolab config

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('./public'));
 
 // mongoose setup
-mongoose.connect('mongodb://localhost/nytreact'); 
+// mongoose.connect('mongodb://localhost/nytreact'); 
+mongoose.connect('mongodb://heroku_g63k1p0l:qkv6liv1cb5an1o3ufrcnjep1n@ds159662.mlab.com:59662/heroku_g63k1p0l');
+
 var db = mongoose.connection;
 db.on('error', function(err) {
   console.log('mongoose error: ', err);
