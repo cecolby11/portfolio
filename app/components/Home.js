e4c5f1f3e1b95fbeb20c4a452f907f013f57906b
cecolby11 <cecolby11@users.noreply.github.com>
2017-07-26 14:10:10 -0500

    content

--- app/components/Home.js
+++ app/components/Home.js
@@ -36,7 +36,7 @@ class Home extends Component {
                   <h4>@school</h4>
                 </Grid.Column>
                 <Grid.Column computer={13} tablet={16}>
-                  <p>Making the leap towards full time web development: Learning to learn and problem-solve in any language like a programmer, write good code, and develop web applications with JavaScript, HTML, CSS, and Node.js in Northwestern University&#39;s full-stack coding bootcamp.</p>
+                  <p>Making the leap towards full time web development: Learning to learn and problem-solve in any language like a programmer, write good code, and develop MERN stack web applications in Northwestern University&#39;s full-stack coding bootcamp.</p>
                 </Grid.Column>
               </Grid.Row>
               <Grid.Row className='about-text'>
