261eab886b03f1ab90e4f2bd91788bf8a8d6d8f8
cecolby11 <cecolby11@users.noreply.github.com>
2017-08-01 20:46:43 -0500

    mongo for web

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('./public'));
 
 // mongoose setup
-mongoose.connect('mongodb://localhost/projects'); 
-// mongoose.connect('mongodb://heroku_g63k1p0l:qkv6liv1cb5an1o3ufrcnjep1n@ds159662.mlab.com:59662/heroku_g63k1p0l');
+// mongoose.connect('mongodb://localhost/projects'); 
+mongoose.connect('mongodb://heroku_g63k1p0l:qkv6liv1cb5an1o3ufrcnjep1n@ds159662.mlab.com:59662/heroku_g63k1p0l');
 
 var db = mongoose.connection;
 db.on('error', function(err) {
